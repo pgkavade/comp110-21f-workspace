7520edce3eff6efb4f2bbd708f84aaef28d55f2e
Preethi Kavadekar <pgkavade@ad.unc.edu>
2021-09-26 22:56:26 -0400

    Progress on exercise 4

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -6,53 +6,53 @@ __author__ = "730395347"
 # TODO: Implement your functions here.
 
 
-def all(match: int, nums: list[int]) -> bool:
+def all(nums: list[int], match: int) -> bool:
     """Given a list of ints and an int, all should return a bool indicating whether or not all the ints in the list are the same as the given int."""
     i: int = 0
     x: int = 0
-    while i < len(nums):
-        if match == nums:
-            i += 1
-            x += 1
-        else:
-            i += 1
-    if x == 3:
-        return True
-    else:
+    if len(nums) == 0:
         return False
+    else:
+        while i < len(nums):
+            if match == nums[i]:
+                i += 1
+                x += 1
+            else:
+                i += 1
+        if x == len(nums):
+            return True
+        else:
+            return False
 
 
-def isequal(nums: list[int], nums1: list[int]) -> bool:
+def is_equal(nums: list[int], nums1: list[int]) -> bool:
+    """Return True if every element at every index is equal in both lists."""
     i: int = 0
     x: int = 0
-    while i < len(nums):
-        if nums[i] == nums1[i]:
-            i += 1
-            x += 1
-        else:
-            i += 1
-    if x == 3:
-        return True
-    else:
+    if len(nums) != len(nums1):
         return False
+    else:
+        while i < len(nums):
+            if nums[i] == nums1[i]:
+                i += 1
+                x += 1
+            else:
+                i += 1
+        if x == len(nums):
+            return True
+        else:
+            return False
 
 
 def max(nums: list[int]) -> int:
+    """Return the largest in the List."""
     if len(nums) == 0:
-        raise ValueError("max() arg is an empty List")
+        raise ValueError("max()arg is an empty List")
     else:
-        i: int = 0
-        x: int = 0
-        while i < len(nums):
-            if x < nums[i]:
-                x == nums[i]
-            i += 1
-    return i
+        i: int = nums[0]
+        for x in nums:
+            if (x > i):
+                i = x   
+        return i
 
-
-nums: list[int] = [1, 2, 3]
-nums1: list[int] = [1, 2, 3]
-match: int = 3
-print(all(match, nums))
-print(isequal(nums, nums1))
-print(max(nums))
+ 
