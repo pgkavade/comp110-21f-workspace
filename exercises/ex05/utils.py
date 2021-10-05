0fd75db710d6f0641254e8435d739af5b7991320
Preethi Kavadekar <pgkavade@ad.unc.edu>
2021-10-05 03:30:54 -0400

    Progress on Exercise 5

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,47 @@
+"""List utility functions part 2."""
+
+__author__ = "730395347"
+
+# Define your functions below
+
+
+def only_evens(list1: list[int]):
+    """Return a list containing only the elements of the input list that were even."""
+    list2: list[int] = []
+    i: int = 0
+    if len(list1) == 0:
+        return list2
+    else:
+        while i < (len(list1)):
+            if (list1[i] % 2) == 0:
+                list2.append(list1[i])
+                i += 1
+            else:
+                i += 1
+        return list2
+
+
+def sub(list1: list[int], x: int, y: int):
+    """Generates a List which is a subset of the given list, between the specified start index and the end index - 1."""
+    list2: list[int] = []
+    if len(list1) == 0:
+        return list2
+    if x & y > len(list1):
+        return list2
+    if x & y < 0:
+        return list2
+    else:
+        list2 = list1[x:y]
+        return list2
+
+
+def concat(list1: list[int], list2: list[int]):
+    """Generates a new List which contains all of the elements of the first list followed by all of the elements of the second list."""
+    list3: list[int] = []
+    if len(list1) == 0 | len(list2) == 0:
+        return list3
+    if len(list2) == 0 | len(list1) == 0:
+        return list3
+    else:
+        list3 = list1 + list2
+        return list3 
